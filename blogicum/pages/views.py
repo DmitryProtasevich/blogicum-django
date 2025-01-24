263679727705e7b54e94fa24b27d204f29338fb1
DmitryProtasevich <protas.dmitry4@yandex.ru>
2025-01-24 14:37:53 +0300

    fix_after_review2

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView, CreateView
 from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render
 from django.urls import reverse_lazy
+from django.views.generic import CreateView, TemplateView
 
 
 class RegistrationCreateView(CreateView):
